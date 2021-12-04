be4132e72f5a23e6d14cb4f4f06de2a063b06a15
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-04 20:13:46 +0100

    Finish day 1

--- D1/Program.cs
+++ D1/Program.cs
@@ -11,9 +11,18 @@ namespace D1
             string path = @"input.txt";
             string[] lines = System.IO.File.ReadAllLines(path);
 
-            foreach(string s in lines) {
-                Console.WriteLine(s);
+            int[] depths = Array.ConvertAll(lines, int.Parse); 
+            
+            int n = depths.Length;
+            int count = 0;
+
+            for (int i = 0; i<n-1; i++) {
+                if (depths[i] < depths[i+1]) {
+                    count++;
+                }
             }
+
+            Console.WriteLine(count);
         }
     }
 }
