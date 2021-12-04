0ae20e8cffb280df6370a4426736fcb622957d1d
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-04 20:06:48 +0100

    initial commit

---
+++ D1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace D1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            foreach(string s in lines) {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
