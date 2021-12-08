864201a92806107c8b8914a9a8a25231b4f05ce4
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-08 14:33:05 +0100

    remove useless code

--- D8/DigitDecoder.cs
+++ D8/DigitDecoder.cs
@@ -12,55 +12,45 @@ namespace D8
 
         public void Solve() {
             Digit[] newDigits = new Digit[10];
-            bool[] skip = new bool[10];
 
             // Find 1, 4, 7, and 8
             for (int i = 0; i < 10; i++) {
-                if (!skip[i]) {
-                    switch(digits[i].Count()) {
-                        case 2:
-                            newDigits[1] = digits[i];
-                            skip[i] = true;
-                            break;
-                        case 3:
-                            newDigits[7] = digits[i];
-                            skip[i] = true;
-                            break;
-                        case 4:
-                            newDigits[4] = digits[i];
-                            skip[i] = true;
-                            break;
-                        case 7:
-                            newDigits[8] = digits[i];
-                            skip[i] = true;
-                            break;
-                        default:
-                            break;
-                    }
+                switch(digits[i].Count()) {
+                    case 2:
+                        newDigits[1] = digits[i];
+                        break;
+                    case 3:
+                        newDigits[7] = digits[i];
+                        break;
+                    case 4:
+                        newDigits[4] = digits[i];
+                        break;
+                    case 7:
+                        newDigits[8] = digits[i];
+                        break;
+                    default:
+                        break;
                 }
             }
 
             // Find 0, 6, and 9
             for (int i = 0; i < 10; i++) {
-                if (!skip[i] && digits[i].Count() == 6) {
+                if (digits[i].Count() == 6) {
                     if ((digits[i] | newDigits[1]) != digits[i]) {
                         newDigits[6] = digits[i];
-                        skip[i] = true;
                     }
                     else if ((digits[i] | newDigits[4]) != digits[i]) {
                         newDigits[0] = digits[i];
-                        skip[i] = true;
                     }
                     else {
                         newDigits[9] = digits[i];
-                        skip[i] = true;
                     }
                 }
             }
 
             // Find 2, 3, and 5
             for (int i = 0; i < 10; i++) {
-                if (!skip[i] && digits[i].Count() == 5) {
+                if (digits[i].Count() == 5) {
                     if ((digits[i] | newDigits[7]) == digits[i]) {
                         newDigits[3] = digits[i];
                     }
