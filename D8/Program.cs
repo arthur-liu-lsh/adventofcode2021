6fd31b6d4c934885fae75e7647bbf81feedfc1ca
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-08 10:29:41 +0100

    day 8 initial commit

---
+++ D8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace D8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            int n = lines.Length;
+            int m = lines[0].Split(new char[] {' ', '|'}, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            string[][] inputs = lines.Select(x => x.Split(new char[] {' ', '|'}, StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+            
+        }
+    }
+}
