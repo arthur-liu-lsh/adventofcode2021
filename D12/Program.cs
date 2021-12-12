2b079047e43eecdea2720046e030da41ceee0076
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-12 08:24:14 +0100

    day 12 initial commit

---
+++ D12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace D12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            
+        }
+    }
+}
