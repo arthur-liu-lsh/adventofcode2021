05524a1c2cee3f89f4fa92697ae32b4967a05875
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-16 08:23:09 +0100

    day 16 initial commit

---
+++ D16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace D16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            int n = lines[0].Length;
+            string messageHex = lines[0];
+        }
+    }
+}
