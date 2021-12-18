95caafb8366f856700243fc1df6efd1284ed4302
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2021-12-18 09:04:56 +0100

    day 18 initial commit

---
+++ D18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
